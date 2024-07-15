323cd6a7900cf9acb6f924cdbd9cd38f50cdbc13
Raj Shah <rajpshah151@gmail.com>
2024-07-15 13:42:12 +0530

    minor changes -- support.js

--- src/src/components/support.js
+++ src/src/components/support.js
@@ -1,6 +1,5 @@
 import React, { useRef } from 'react';
 import emailjs from 'emailjs-com';
-// import '../App.css' ;
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
